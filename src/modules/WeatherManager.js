a0198ae2a0e41b602af87b85b22e055b532103b9
David Larsen <davidplarsen@gmail.com>
2020-07-24 10:01:53 -0500

    Initialize API Manager fileset

---
+++ src/modules/WeatherManager.js
@@ -0,0 +1,10 @@
+/* This module contains all the fetch calls pertaining
+to the external WeatherStack API */
+
+// WeatherStack API URL
+const url = "https://api.weatherstack.com"
+const key = process.env.REACT_APP_WEATHERSTACK_KEY
+
+// Fetch calls
+
+
