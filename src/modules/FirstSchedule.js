6b92b5b0e746ae805e57c021313c6b6fe43b9c6b
David Larsen <davidplarsen@gmail.com>
2020-08-03 16:40:44 -0500

    Add end date to reminders

--- src/modules/FirstSchedule.js
+++ src/modules/FirstSchedule.js
@@ -15,27 +15,32 @@ export function FirstSchedule() {
     const userId = parseInt(sessionStorage.credentials)
 
     // Assign dethatch
-    const dethatchDate = (new Date(`${currentYear}-03-15`) > today) ? `${currentYear}-03-15` : `${parseInt(currentYear) + 1}-03-15`
-    const schedule = [{userid: userId, activityId: 6, date: dethatchDate }]
+    const dethatchYear = (new Date(`${currentYear}-03-15`) > today) ? currentYear : parseInt(currentYear) + 1
+    const schedule = [{userid: userId, activityId: 6, startDate: `${dethatchYear}-03-15`, endDate: `${dethatchYear}-03-31`}]
 
     // Assign weed control
-    const weedDate = (new Date(`$userid: userId,currentYear}-05-01`) > today) ? `${currentYear}-05-01` : `${parseInt(currentYear) + 1}-05-01`
-    schedule.push({userid: userId, activityId: 5, date: weedDate })
+    const weedYear = (new Date(`${currentYear}-05-01`) > today) ? currentYear : parseInt(currentYear) + 1
+    schedule.push({userid: userId, activityId: 5, startDate: `${weedYear}-05-01`, endDate: `${weedYear}-05-31` })
 
     // Assign aerate
-    const aerateDate = (new Date(`${currentYear}-09-01`) > today) ? `${currentYear}-09-01` : `${parseInt(currentYear) + 1}-09-01`
-    schedule.push({userid: userId, activityId: 8, date: aerateDate })
+    const aerateYear = (new Date(`${currentYear}-09-01`) > today) ? currentYear : parseInt(currentYear) + 1
+    schedule.push({userid: userId, activityId: 8, startDate: `${aerateYear}-09-01`, endDate: `${aerateYear}-09-17`})
 
     // Assign seed x 2
-    const seed1 = (new Date(`${currentYear}-03-15`) > today) ? `${currentYear}-03-15` : `${parseInt(currentYear) + 1}-03-15`  
-    const seed2 = (new Date(`${currentYear}-09-01`) > today) ? `${currentYear}-09-01` : `${parseInt(currentYear) + 1}-09-01`
-    schedule.push({userid: userId, activityId: 6, date: seed1}, {userid: userId, activityId: 6, date: seed2})
+    const seed1 = (new Date(`${currentYear}-03-15`) > today) ? currentYear : parseInt(currentYear) + 1  
+    const seed2 = (new Date(`${currentYear}-09-01`) > today) ? currentYear : parseInt(currentYear) + 1
+    schedule.push({userid: userId, activityId: 6, startDate: `${seed1}-03-15`, endDate: `${seed1}-03-31`},
+        {userid: userId, activityId: 6, startDate: `${seed2}-09-01`, endDate: `${seed2}-09-15`}
+    )
 
     // Assign fertilize x 3
     const fertilize1 = (new Date(`${currentYear}-04-01`) > today) ? `${currentYear}-04-01` : `${parseInt(currentYear) + 1}-04-01`
     const fertilize2 = (new Date(`${currentYear}-07-01`) > today) ? `${currentYear}-07-01` : `${parseInt(currentYear) + 1}-07-01`
     const fertilize3 = (new Date(`${currentYear}-10-01`) > today) ? `${currentYear}-10-01` : `${parseInt(currentYear) + 1}-10-01`
-    schedule.push({userid: userId, activityId: 4, date: fertilize1}, {userid: userId, activityId: 4, date: fertilize2}, {userid: userId, activityId: 4, date: fertilize3})
+    schedule.push({userid: userId, activityId: 4, startDate: `${fertilize1}-04-01`, endDate: `${fertilize1}-04-30`}, 
+        {userid: userId, activityId: 4, startDate: `${fertilize2}-07-01`, endDate: `${fertilize2}-07-31`},
+        {userid: userId, activityId: 4, startDate: `${fertilize3}-10-01`, endDate: `${fertilize3}-10-31`}
+    )
 
     let savedSchedule = []
     schedule.forEach(reminder => {
