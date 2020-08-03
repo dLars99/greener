33e9d59bd27770062ce0bdf74aeb05c1e3dd7cf0
David Larsen <davidplarsen@gmail.com>
2020-08-03 13:28:16 -0500

    Complete forecast

--- src/components/weather/ForecastCard.js
+++ src/components/weather/ForecastCard.js
@@ -19,17 +19,17 @@ const ForecastCard = (props) => {
                         <h4>{props.weather.day.condition.text}</h4>
                     </div>
                     <div className="forecast--temp">
-                        <h3>{parseInt(props.weather.day.maxtemp_f)}&#176;</h3>
-                        <h3>{parseInt(props.weather.day.mintemp_f)}&#176;</h3>
+                        <h3>Hi {parseInt(props.weather.day.maxtemp_f)}&#176;</h3>
+                        <h3>Lo {parseInt(props.weather.day.mintemp_f)}&#176;</h3>
                     </div>
                 </div>
                 <div className="forecast--health">
-                    Percent change
-                    UV
+                    <p>Rain chance: {parseInt(props.weather.day.daily_chance_of_rain)}%</p>
+                    <p>UV Index: {props.weather.day.uv}</p>
                 </div>
                 <div className="forecast--sun">
-                    Sunrise
-                    Sunset
+                    <p>Sunrise {props.weather.astro.sunrise}</p>
+                    <p>Sunset {props.weather.astro.sunset}</p>
                 </div>
             </>
             : <p>Loading Current Weather</p>
