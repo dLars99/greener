2830d43586413b2f3c78c1dc1aea12a3e0c68852
David Larsen <davidplarsen@gmail.com>
2020-08-04 18:56:20 -0500

    Full year verification fixes

--- src/components/reminders/Schedulers.js
+++ src/components/reminders/Schedulers.js
@@ -2,42 +2,48 @@ import DatabaseManager from "../../modules/DatabaseManager"
 import { convertDate } from "../../modules/Helpers"
 
 // Check activity categories one-by-one to make sure the correct number of reminders exists for the full year
-export function CheckFullYear(reminders, entries) {
+export function CheckFullYear(reminders) {
+    console.log(reminders)
+    DatabaseManager.getAll("activities").then(activitiesList => {
 
-    const activitiesToAdd = entries.filter(activity => {
-        const reminderCount = reminders.reduce((count, cur) => {
-            if (cur.activityId === activity.id) {
-                count++
+            const activitiesToAdd = activitiesList.filter(activity => {
+                const reminderCount = reminders.reduce((count, cur) => {
+                    if (cur.activityId === activity.id) {
+                    count++
+                    }
+                    return count
+                }, 0)
+            console.log(reminderCount, activity.repeat)
+            return reminderCount < activity.repeat
+            })
+        
+        console.log(activitiesToAdd)
+        // Sort by date, newest on top
+        const sortedReminders = reminders.sort((a, b) => new Date(a.date) - new Date(b.date))
+        let isUpdated = false
+        let savedActivities = []
+        // Set date for next occurrence of needed activities and save to database
+        activitiesToAdd.forEach(activity => {
+            const lastReminder = sortedReminders.find(reminder => reminder.activityId = activity.id)
+            // Add the interval number of days to the last occurrence to get the new date
+            let newDate = new Date(`${lastReminder.startDate}T00:00:00`).getTime() + (86400000 * activity.interval)
+            let dateString = convertDate(newDate)
+            const thisYear = new Date().getFullYear()
+            if (newDate > new Date(`11/01/${parseInt(thisYear)}`).getTime() && newDate < new Date(`03/15/${parseInt(thisYear + 1)}`).getTime()) {
+                dateString = `${parseInt(thisYear + 1)}-${activity.firstAnnualDate}`
+            } else {
+                dateString = convertDate(newDate)
             }
+            // Add days to newDate to get window end date
+            const newEndDate = convertDate(new Date(`${dateString}T00:00:00`).getTime() + (86400000 * activity.window))
+            const activityToSave = { userId: parseInt(sessionStorage.credentials), activityId: activity.id, startDate: dateString, endDate: newEndDate  } 
+            savedActivities.push(DatabaseManager.addNew("reminders", activityToSave))
+            isUpdated = true
         })
-        return reminderCount < activity.count
+        
+        console.log("Checked for full year", isUpdated)
+        return Promise.all(savedActivities).then(() => isUpdated)
     })
-
-    // Sort by date, newest on top
-    const sortedReminders = reminders.sort((a, b) => new Date(a.date) - new Date(b.date))
-    let isUpdated = false
-    let savedActivities = []
-    // Set date for next occurrence of needed activities and save to database
-    activitiesToAdd.forEach(activity => {
-        const lastReminder = sortedReminders.find(reminder => reminder.activityId = activity.id)
-        // Add the interval number of days to the last occurrence to get the new date
-        let newDate = new Date(`${lastReminder.startDate}T00:00:00`).getTime() + (86400000 * activity.interval)
-        let dateString = convertDate(newDate)
-        const thisYear = new Date().getFullYear()
-        if (newDate > new Date(`11/01/${parseInt(thisYear)}`).getTime() && newDate < (`03/15/${parseInt(thisYear + 1)}`).getTime()) {
-            dateString = `${parseInt(thisYear + 1)}-${activity.firstAnnualDate}`
-        } else {
-            dateString = convertDate(newDate)
-        }
-        // Add days to newDate to get window end date
-        const newEndDate = convertDate(new Date(`${newDate}T00:00:00`).getTime() + (86400000 * window))
-        const activityToSave = { userId: sessionStorage.credentials, activityId: activity.id, startDate: dateString, endDate: newEndDate  } 
-        savedActivities.push(DatabaseManager.addObject("reminders", activityToSave))
-        isUpdated = true
-    })
-
-    console.log("Checked for full year", isUpdated)
-    return Promise.all(savedActivities).then(() => isUpdated)
 
 }
 
@@ -60,8 +66,6 @@ export function CheckElapsed(reminders) {
 }
 
 export function CheckForRecentEntry(reminders, entries) {
-    console.log("Reminders", reminders)
-    console.log("Entries", entries)
     let isRecent = false
     const recent = reminders.filter(reminder => 
         entries.some(entry => new Date(entry.date) >= new Date(reminder.startDate) && entry.activities.some(activity => activity.id === reminder.activityId))
