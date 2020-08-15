1f4197237b077e21aa8780c1a864aa4f68ef18fa
David Larsen <davidplarsen@gmail.com>
2020-08-14 22:32:15 -0500

    Fix registration field count after login attempt bug

--- src/modules/Validate.js
+++ src/modules/Validate.js
@@ -51,7 +51,7 @@ export function VerifyReg1(newUser, existingUsers) {
 
     // 1. Verify that all signin fields are complete; return an error if not
     if (Object.values(newUser).some(field => field === null || field === '' || field === undefined) ||
-        Object.keys(newUser).length !== 4) {
+        Object.keys(newUser).length < 4) {
         return "Please fill out all four fields"
     }
 
